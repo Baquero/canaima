4ac3ed9491d724179cf030402f83f68683a98e17
root <root@canaima-popular-pc.(none)>
2014-07-03 11:42:21 -0430

    Añadiendo el script David.sh

---
+++ David.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+debmirror --host=ftp.debian.org --root=debian --cleanup --nosource --progress --ignore-release-gpg --arch=i386 --dist=stable --method=http --section=main /home/canaima-popular/debian
